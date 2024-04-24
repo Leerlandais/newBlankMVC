52822c5287b03461c6e795911522cbc5711e2661
leerlandais <leerlandais@gmail.com>
2024-04-24 18:27:58 +0200

    small correction of homepage

--- view/homeView.php
+++ view/homeView.php
@@ -10,7 +10,7 @@
     <title><?=$title?></title>
 </head>
 <body>
-    
+    <div class="container"> <!-- CONTAINER GLOBAL POUR ENGLOBÉ LE TOTALE -->
     <?php
         // INCLURE SI NECESSAIRE (BON ENDROIT POUR LE NAVBAR PAR EXEMPLE)
         include("inc/header.php");
@@ -28,7 +28,7 @@
         // INCLURE SI NECESSAIRE
         include("inc/footer.php");
     ?>
-    
+    </div>
     <!-- BOOTSTRAP CDN[SCRIPTS] ICI -->    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- ET PUIS STYLE PERSO -->
